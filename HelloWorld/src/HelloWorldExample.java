c12a09513de8483062d195f941cde28c58cc2f70
Saumya <Saumya@pnut.net4nuts.local>
2016-05-06 16:14:34 +0530

    Hello World Example

---
+++ HelloWorld/src/HelloWorldExample.java
@@ -0,0 +1,14 @@
+
+public class HelloWorldExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("hello World!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
