a9d6aaacea6992396a86edcdce9f1240a6e8ba4e
Saumya <Saumya@pnut.net4nuts.local>
2016-05-07 10:37:42 +0530

    hi

--- HelloWorld/src/HelloWorldExample.java
+++ HelloWorld/src/HelloWorldExample.java
@@ -1,17 +1,12 @@
+
 public class HelloWorldExample {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
 
-		System.out.println("hello World!");
-		System.out.println("Testing!!");
-		System.out.println("Testing!!!");
-		System.out.println("Hello");
-		System.out.println("Test1");
-		System.out.println();
+	public static void main(String[] args) {
 		
+		System.out.println("hello World!");
+		System.out.println("Hello Github!");
+		System.out.println("M");
 
 	}
 
