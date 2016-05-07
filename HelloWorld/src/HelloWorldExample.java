9e362fecba3988ff274afc9b75412eaf402261fd
Saumya <Saumya@pnut.net4nuts.local>
2016-05-07 11:55:01 +0530

    Test

--- HelloWorld/src/HelloWorldExample.java
+++ HelloWorld/src/HelloWorldExample.java
@@ -5,9 +5,7 @@ public class HelloWorldExample {
 	public static void main(String[] args) {
 		
 		System.out.println("hello World!");
-		System.out.println("Hello Github!");
-		System.out.println("M");
-		System.out.println("Switching To mater");
+		System.out.println("Testing Commencing");
 
 	}
 
