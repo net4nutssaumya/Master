3805c50d053fa9e45ac6216360f2354e703ac76f
Saumya <Saumya@pnut.net4nuts.local>
2016-05-09 12:45:09 +0530

    Spaces removed

--- HelloWorld/src/HelloWorldExample.java
+++ HelloWorld/src/HelloWorldExample.java
@@ -1,15 +1,8 @@
-
 public class HelloWorldExample {
-
-
 	public static void main(String[] args) {
-		
-		System.out.println("hello World!");
-		
+		System.out.println("hello World!");		
 		System.out.println("Switching To mater");
-
 	}
-
 }
 
 
